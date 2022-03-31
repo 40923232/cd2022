2d06c36cd9ba2e076b0e1ba79e4996e23a20ba61
40923232 <40923232@gm.nfu.edu.tw>
2022-03-31 15:51:16 +0800

    mtest

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR ='40923232'
+SITENAME = '40923232 w6 blog'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
